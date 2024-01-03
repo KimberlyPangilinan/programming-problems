2a9fffa0165c014c25d485caf8e64199b5de811b
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2024-01-03 13:03:52 +0800

    feat: isogram checker

---
+++ 1_isogram/index.js
@@ -0,0 +1,13 @@
+function isIsogram(str) {
+    str = str.toLowerCase()
+    for (let i=0; i < str.length; i++){
+        if (str.indexOf(str[i]) !== i) {
+            console.log(`${str} is not an isogram`)
+            return false;
+        }
+    }
+    console.log(`${str} is an isogram`)
+    return true;
+}
+isIsogram("hello")
+isIsogram("Kim")
